fa6cee0c2f8563a31a61c0e69f46cee0a94ada62
Eva1245 <43342369+Eva1245@users.noreply.github.com>
2018-11-05 16:56:48 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Second commit");
         System.out.println("Next Commit");
         
-        System.out.println("branch2");
+        System.out.println("master");
        
     }
 }
